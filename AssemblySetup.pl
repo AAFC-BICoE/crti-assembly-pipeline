d4487d4d4baf27eb4586b7377c0cdaf0ca0408a3
Jeff Cullis <jeffcullis@gmail.com>
2013-08-06 20:08:23 +0000

    JC: last update before attempting to fix bug 2274 (wide range of effects throughout pipeline
    git-svn-id: http://svn.biodiversity.agr.gc.ca/repo/source/AssemblyPipeline@3808 8f470391-0214-0410-be57-dbd57b47fce5

--- AssemblySetup.pl
+++ AssemblySetup.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/env per
+#!/usr/bin/env perl
 use strict;
 use warnings;
 use Getopt::Long;
@@ -8,16 +8,9 @@ use Assembly::Util;
 sub set_default_opts
 {
     my %defaults = qw(
-        yaml_in yaml_files/09_velvet_cmds.yml
-        yaml_out yaml_files/10_velveth_qsub_host.yml
-        job_table_in input_data/VHQsubJobIDs.tab
-        job_table_out output_files/VHQsubJobIDsOut.tab
-        qsub_script qsub_script.sh
-        trim 1
-        raw 1
+        yaml_in yaml_files/05_readinfo.yml
+        yaml_out yaml_files/06_assembly_setup.yml
         verbose 0
-        submit 0
-        submit_max 0
         );
     for my $kdef (keys %defaults) {
         $options->{$kdef} = $defaults{$kdef} unless $options->{$kdef};
@@ -31,13 +24,6 @@ sub check_opts
         die "Usage: $0 -i <input yaml file> -o <output yaml file>
             Optional:
                 --verbose
-                --sample_list <ID1,ID2,...,IDX (no spaces)>
-                --qsub_script <filename>
-                --memory
-                --trim
-                --raw
-                --submit
-                --submit_max <max # jobs to qsub>
                 ";
     }
 }
@@ -48,13 +34,7 @@ sub gather_opts
     GetOptions($options,
         'yaml_in|i=s',
         'yaml_out|o=s',
-        'qsub_script=s',
         'verbose',
-        'sample_list=s',
-        'trim',
-        'raw',
-        'submit',
-        'submit_max=s',
         );
     set_default_opts;
     check_opts;
@@ -86,7 +66,8 @@ sub genomic_super_rec
         my $species = $rec->{species};
         my $strain = get_check_record($rec, ["sequence_metadata", "Genotype"]);
         my $ga_key = get_genomic_key($species, $strain);
-        $genomic_super{$ga_key}{samples}{$sample} = $rec;
+        set_check_record($genomic_super, [$ga_key, "samples"], $sample, $rec);
+        #$genomic_super{$ga_key}{samples}{$sample} = $rec;
     }
     return $genomic_super;
 }
