14ea5474c4f6ecd357125361504a9d36ac5425a8
Jeff Cullis <jeffcullis@gmail.com>
2013-09-18 21:56:39 +0000

    JC bug 2404
    git-svn-id: http://svn.biodiversity.agr.gc.ca/repo/source/AssemblyPipeline@4067 8f470391-0214-0410-be57-dbd57b47fce5

--- ReleaseGenomes.pl
+++ ReleaseGenomes.pl
@@ -2,21 +2,23 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use YAML::XS;
+use YAML::XS qw(LoadFile DumpFile);
 use POSIX qw(strftime);
 use Assembly::Utils;
 use Cwd;
+use File::Path;
 
 my $options = {};
-my @release_headers = qw (Species Strain Sequencing_Types Trim_Raw Release_Version Best_Kmer Best_N50);
+my @release_headers = qw (Species Strain Sequencing_Types Trim_Raw Release_Version Est_Genome_Size Best_Kmer Best_N50);
 
 sub set_default_opts
 {
     my %defaults = qw(
-        yaml_in yaml_files/12_velvet_advisor.yml
+        yaml_in yaml_files/12_velvet_stats.yml
         yaml_out yaml_files/13_velvet_release.yml
         release_table input_data/Release.tab
-        verbose 0
+        run 0
+        verbose 1
     );
     for my $kdef (keys %defaults) {
         $options->{$kdef} = $defaults{$kdef} unless $options->{$kdef};
@@ -29,6 +31,7 @@ sub check_opts
         die "Usage: $0 -i <input yaml file> -o <output yaml file> -r <release table>
             Optional:
                 --verbose
+                --run (create directories and yaml files and copy release data)
             ";
     }
 }
@@ -41,7 +44,17 @@ sub gather_opts
         'yaml_out|o=s',
         'release_table|r=s',
         'verbose',
+        'run',
         );
+    set_default_opts;
+    check_opts;
+}
+
+sub print_verbose
+{
+    if ($options->{verbose}) {
+        print (@_);
+    }
 }
 
 sub get_software_versions
@@ -76,9 +89,11 @@ sub parse_release_table
         }
         my $species = Assembly::Utils::format_species_key($rec->{Species});
         my $strain = Assembly::Utils::format_strain_key($rec->{Strain});
-        $release_candidates->{$species} = {};
+        $release_candidates->{$species} = {} unless $release_candidates->{$species};
         $release_candidates->{$species}->{$strain} = $rec;
+        #print "Added release candidate record for species $species strain $strain\n";
     }
+    close (RTAB);
     return $release_candidates;
 }
 
@@ -87,9 +102,9 @@ sub get_release_prefix
     my $species = shift;
     my $strain = shift;
     my $cand_rec = shift;
-    my $yaml_rec = shift;
+    my $strain_rec = shift;
     
-    my $species_abbr = Assembly::Utils::get_check_record($yaml_rec, [$species, "DNA", $strain, "PE", "species_abbr"]);
+    my $species_abbr = Assembly::Utils::get_check_record($strain_rec, ["PE", "species_abbr"]);
     my $cand_release_version = $cand_rec->{Release_Version};
     my $release_prefix = $species_abbr . "_" . $strain . "_" . $cand_release_version;
     return $release_prefix;
@@ -133,7 +148,7 @@ sub get_genome_stanza
     my $release_kmer = shift;
     my $outrec = {};
     $outrec->{species_dir} = Assembly::Utils::get_check_record($inrec, ["PE", "species_dir"]);
-    $outrec->{estimated_genome_length} = Assembly::Utils::get_check_record($inrec, ["PE", "related_genome_length", "RG_Est_Genome_Length"]);
+    $outrec->{estimated_genome_length} = Assembly::Utils::get_check_record($inrec, ["related_genome_length", "RG_Est_Genome_Length"]);
     $outrec->{release_kmer} = $release_kmer;
     $outrec->{N50} = Assembly::Utils::get_check_record($inrec, ["velvet", $trimraw, "kmer", $release_kmer, "N50"]);
     $outrec->{max_contig} = Assembly::Utils::get_check_record($inrec, ["velvet", $trimraw, "kmer", $release_kmer, "max_contig"]);
@@ -151,9 +166,10 @@ sub get_genome_stanza
 
 sub get_sample_stanza
 {
-    my $sample_rec = shift;
+    my $strain_rec = shift;
     my $sample_type = shift;
     my $trimraw = shift;
+    my $sample_rec = Assembly::Utils::get_check_record($strain_rec, [$sample_type]);
     my $outrec = {};
     $outrec->{sequencing_metadata} = Assembly::Utils::get_check_record($sample_rec, ["sequencing_metadata"]);
     # $outrec->{species_abbr} = Assembly::Utils::get_check_record($sample_rec, ["species_abbr"]);
@@ -173,8 +189,8 @@ sub get_sample_stanza
     # get info for output file path for release raw reads
     my $bio_type = Assembly::Utils::get_check_record($sample_rec, ["bio_type"]);
     my $sample_id = Assembly::Utils::get_check_record($sample_rec, ["sample"]);
-    my $release_prefix = Assembly::Utils::get_check_record($sample_rec, ["release", "prefix"]);
-    my $release_dir = Assembly::Utils::get_check_record($sample_rec, ["release", "release_dir"]);
+    my $release_prefix = Assembly::Utils::get_check_record($strain_rec, ["release", "prefix"]);
+    my $release_dir = Assembly::Utils::get_check_record($strain_rec, ["release", "release_dir"]);
     my $fpath_base = $release_dir . "/" . $release_prefix . "_" . $bio_type . "_" . $sample_type . 
             "_" . $sample_id . "_";
 
@@ -193,10 +209,16 @@ sub get_sample_stanza
 
     # copy the files here??
     unless (-e $r1_out and (-s $r1_in) == (-s $r1_out)) {
-        #system("cp $r1_in $r1_out");
+        print_verbose "cp $r1_in $r1_out\n";
+        if ($options->{run}) {
+            system("cp $r1_in $r1_out");
+        }
     }
     unless (-e $r2_out and (-s $r2_in) == (-s $r2_out)) {
-        #system("cp $r2_in $r2_out");
+        print_verbose "cp r2_in $r2_out\n";
+        if ($options->{run}) {
+            system("cp $r2_in $r2_out");
+        }
     }
 
     if ($trimraw =~ /trim/) {
@@ -210,9 +232,9 @@ sub get_sample_stanza
         $outrec->{read_data}->{R2}->{trim}->{read_length} = Assembly::Utils::get_check_record($sample_rec, ["data_stats", "R2", "trimdata", "read_length"]);
 
         my $tr1_in = Assembly::Utils::get_check_record($sample_rec, ["R1", "trimdata"]);
-        my $tr1_out = $fpath_base . "R1.fq";
+        my $tr1_out = $fpath_base . "trim_R1.fq";
         my $tr2_in = Assembly::Utils::get_check_record($sample_rec, ["R2", "trimdata"]);
-        my $tr2_out = $fpath_base . "R2.fq";
+        my $tr2_out = $fpath_base . "trim_R2.fq";
         
         $outrec->{release}->[2] = {};
         $outrec->{release}->[2]->{input_file} = $tr1_in;
@@ -220,12 +242,26 @@ sub get_sample_stanza
         $outrec->{release}->[3] = {};
         $outrec->{release}->[3]->{input_file} = $tr2_in;
         $outrec->{release}->[3]->{output_file} = $tr2_out;
+        
+        unless (-e $tr1_out and (-s $tr1_in) == (-s $tr1_out)) {
+            print_verbose "cp $tr1_in $tr1_out\n";
+            if ($options->{run}) {
+                system("cp $tr1_in $tr1_out");
+            }
+        }
+        unless (-e $tr2_out and (-s $tr2_in) == (-s $tr2_out)) {
+            print_verbose "cp $tr2_in $tr2_out\n";
+            if ($options->{run}) {
+                system("cp $tr2_in $tr2_out");
+            }
+        }
     }
     
     # Now add the fastqc info to the pipeline
     my $qcr1 = {};
     $qcr1->{description} = "FastQC";
     $qcr1->{version} = `fastqc --version`;
+    chomp $qcr1->{version};
     $qcr1->{command} = Assembly::Utils::get_check_record($sample_rec, ["fastqc", "R1", "raw_cmd"]);
     $qcr1->{qsub_cmd} = Assembly::Utils::get_check_record($sample_rec, ["fastqc", "R1", "raw_qsub"]);
     $qcr1->{run_dir} = getcwd;
@@ -233,6 +269,7 @@ sub get_sample_stanza
     my $qcr2 = {};
     $qcr2->{description} = "FastQC";
     $qcr2->{version} = `fastqc --version`;
+    chomp $qcr2->{version};
     $qcr2->{command} = Assembly::Utils::get_check_record($sample_rec, ["fastqc", "R2", "raw_cmd"]);
     $qcr2->{qsub_cmd} = Assembly::Utils::get_check_record($sample_rec, ["fastqc", "R2", "raw_qsub"]);
     $qcr2->{run_dir} = getcwd;
@@ -240,6 +277,7 @@ sub get_sample_stanza
     my $qtr1 = {};
     $qtr1->{description} = "Fastx_trimmer";
     $qtr1->{version} = `fastx_trimmer -h | grep FASTX`;
+    chomp $qtr1->{version};
     $qtr1->{command} = Assembly::Utils::get_check_record($sample_rec, ["fastx_trimmer", "R1", "trim_cmd"]);
     $qtr1->{qsub_cmd} = Assembly::Utils::get_check_record($sample_rec, ["fastx_trimmer", "R1", "qsub_cmd"]);
     $qtr1->{run_dir} = getcwd;
@@ -247,6 +285,7 @@ sub get_sample_stanza
     my $qtr2 = {};
     $qtr2->{description} = "Fastx_trimmer";
     $qtr2->{version} = `fastx_trimmer -h | grep FASTX`;
+    chomp $qtr2->{version};
     $qtr2->{command} = Assembly::Utils::get_check_record($sample_rec, ["fastx_trimmer", "R2", "trim_cmd"]);
     $qtr2->{qsub_cmd} = Assembly::Utils::get_check_record($sample_rec, ["fastx_trimmer", "R2", "qsub_cmd"]);
     $qtr2->{run_dir} = getcwd;    
@@ -255,13 +294,20 @@ sub get_sample_stanza
     my $qct1 = {};
     $qct1->{description} = "FastQC";
     $qct1->{version} = `fastqc --version`;
+    chomp $qct1->{version};
     $qct1->{command} = Assembly::Utils::get_check_record($sample_rec, ["fastqc", "R1", "trim_cmd"]);
     $qct1->{qsub_cmd} = Assembly::Utils::get_check_record($sample_rec, ["fastqc", "R1", "trim_qsub"]);
     $qct1->{run_dir} = getcwd;
+    #$qct1->{release} = [];
+    #$qct1->{release}->[0] = {};
+    #$qct1->{release}->[0]->{input_file} = Assembly::Utils::get_check_record($sample_rec, ["R1", "trimdata"]);
+    #$qct1->{release}->[0]->{output_file} = 
+    # this is handled in the location above... should it be??
     
     my $qct2 = {};
     $qct2->{description} = "FastQC";
     $qct2->{version} = `fastqc --version`;
+    chomp $qct2->{version};
     $qct2->{command} = Assembly::Utils::get_check_record($sample_rec, ["fastqc", "R2", "trim_cmd"]);
     $qct2->{qsub_cmd} = Assembly::Utils::get_check_record($sample_rec, ["fastqc", "R2", "trim_qsub"]);
     $qct2->{run_dir} = getcwd;
@@ -292,6 +338,7 @@ sub get_pipeline_stanza
     $vh->{description} = "velveth";
     $vh->{run_dir} = getcwd;
     $vh->{version} = `velveth_31 | grep Version`;
+    chomp $vh->{version};
     $vh->{command} = Assembly::Utils::get_check_record($krec, ["velveth_cmd"]);
     $vh->{qsub_cmd} = Assembly::Utils::get_check_record($krec, ["velveth_qsub_cmd"]);
     
@@ -299,6 +346,7 @@ sub get_pipeline_stanza
     $vg->{description} = "velvetg";
     $vg->{run_dir} = getcwd;
     $vg->{version} = `velvetg_31 | grep Version`;
+    chomp $vg->{version};
     $vg->{command} = Assembly::Utils::get_check_record($krec, ["velvetg_cmd"]);
     $vg->{qsub_cmd} = Assembly::Utils::get_check_record($krec, ["velvetg_qsub_cmd"]);
     my $kmer_dir = Assembly::Utils::get_check_record($krec, ["kmer_dir"]);
@@ -308,6 +356,17 @@ sub get_pipeline_stanza
     $vg->{release}->[0] = {};
     $vg->{release}->[0]->{input_file} = $contigs_infile;
     $vg->{release}->[0]->{output_file} = $contigs_outfile;
+    
+    unless (-e $contigs_outfile and -s $contigs_outfile) {
+        print_verbose "cp $contigs_infile $contigs_outfile\n";
+        if ($options->{run}) {
+            system ("cp $contigs_infile $contigs_outfile");
+        }
+    }
+    
+    push (@$outrec, $vh);
+    push (@$outrec, $vg);
+    return $outrec;
 }   
 
 sub create_release_yaml
@@ -326,7 +385,7 @@ sub create_release_yaml
     for my $sample_type (qw(PE PER MP MP3 MP8)) {
         my $sample_rec = Assembly::Utils::get_check_record($strain_rec, [$sample_type]);
         if ($sample_rec) {
-            my ($sample_stanza, $qc_cmds) = get_sample_stanza($sample_rec, $sample_type);
+            my ($sample_stanza, $qc_cmds) = get_sample_stanza($strain_rec, $sample_type, $trimraw);
             $release_yaml_rec->{$sample_type} = $sample_stanza;
             for my $rec (@$qc_cmds) {
                 push (@$pipeline, $rec);
@@ -334,7 +393,9 @@ sub create_release_yaml
         }
     }
     # Now get the rest of the pipeline
-    my $pipeline_remainder = get_pipeline_stanza($strain_rec, $trimraw);
+    my $release_prefix = Assembly::Utils::get_check_record($strain_rec, ["release", "prefix"]);
+    my $release_dir = Assembly::Utils::get_check_record($strain_rec, ["release", "release_dir"]);
+    my $pipeline_remainder = get_pipeline_stanza($strain_rec, $trimraw, $release_kmer, $release_dir, $release_prefix);
     for my $rec (@$pipeline_remainder) {
         push (@$pipeline, $rec);
     }
@@ -352,16 +413,20 @@ sub create_release
     my $release_prefix = get_release_prefix($species, $strain, $cand_rec, $yaml_rec);
     my $species_dir = Assembly::Utils::get_check_record($yaml_rec, ["PE", "species_dir"]);
     my $release_dir = $species_dir . "/release/" . $release_prefix;
+    if ($options->{run} and not -e $release_dir) {
+        mkpath $release_dir;
+    }
     Assembly::Utils::set_check_record($yaml_rec, ["release"], "prefix", $release_prefix);
     Assembly::Utils::set_check_record($yaml_rec, ["release"], "release_dir", $release_dir);
     Assembly::Utils::set_check_record($yaml_rec, ["release"], "version", $cand_rec->{Release_Version});
     unless (release_exists($release_dir, $release_prefix)) {
         mkpath $release_dir;
-        my $release_yaml_fname = $release_dir . "/" . $release_prefix . ".yml";
+        #my $release_yaml_fname = $release_dir . "/" . $release_prefix . ".yml";
+        my $release_yaml_fname = "/tmp/" . $release_prefix . "_metadata.yml";
         Assembly::Utils::set_check_record($yaml_rec, ["release"], "yaml_file", $release_yaml_fname);
         my $trimraw = $cand_rec->{Trim_Raw};
         my $release = create_release_yaml($species, $strain, $cand_rec, $yaml_rec);
-        #DumpFile($release_yaml_fname, $release);
+        DumpFile($release_yaml_fname, $release);
         # add yml info to release yml record
         # write the release yml record
         # copy over the required files
@@ -374,10 +439,20 @@ sub create_all_releases
     my $records = shift;
     for my $species (keys %$records) {
         for my $strain (keys %{$records->{$species}->{DNA}}) {
-            my $cand_rec = Assembly::Utils::get_check_record($release_candidates, [$species, $strain]);
+            #my $cand_rec = Assembly::Utils::get_check_record($release_candidates, [$species, $strain]);
+            my $cand_rec = $release_candidates->{$species};
             if ($cand_rec) {
+                #print "Matched species $species\n";
+                #print "strain is '$strain'\n";
+                $cand_rec = $release_candidates->{$species}->{$strain};
+                #print "possible strain values: '" . join ("'\t'", (keys %{$release_candidates->{$species}})) . "'\n";
+            }
+            if ($cand_rec) {
+                #print "Got release candidate for species $species strain $strain\n";
                 my $yaml_rec = Assembly::Utils::get_check_record($records, [$species, "DNA", $strain]);
                 create_release ($species, $strain, $cand_rec, $yaml_rec);
+            } else {
+                #print "No candidate record for species $species strain $strain\n";
             }
         }
     }
