dd739091ac7df70dabbbffb7c34699319503d5b9
Jeff Cullis <jeffcullis@gmail.com>
2014-01-09 21:28:29 +0000

    JC: added first decontamination scripts - see bug 2847
    git-svn-id: http://svn.biodiversity.agr.gc.ca/repo/source/AssemblyPipeline@4470 8f470391-0214-0410-be57-dbd57b47fce5

---
+++ Decontaminate/ContigBlast.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+qsub_path=/opt/gridengine/bin/lx26-amd64/qsub
+
+# g is your contigs file
+# p is number of processors per submitted job
+# s is number of files to generate.
+num_procs=8
+num_splits=100
+while getopts g:c:n: flag; do
+    case $flag in
+      g)
+        genome_file=$OPTARG;
+        ;;
+      c)
+        num_procs=$OPTARG;
+        ;;
+      n)
+        num_splits=$OPTARG;
+        ;;
+      ?)
+        exit;
+        ;;
+    esac
+done
+blast_working_dir=`echo $genome_file | sed 's/\..*/_work/'`
+mkdir -p $blast_working_dir
+
+# Setup and run the blast identification, given an input contigs file.
+svn export http://biodiversity/svn/source/BlastParallel/parallel-blast.pl
+svn export http://biodiversity/svn/source/misc_scripts/parseBlastOutput.pl
+
+chmod +x parallel-blast.pl
+chmod +x parseBlastOutput.pl
+
+# Run parallel-blast.pl using the contigs file as input. 
+merge_jobid=`./parallel-blast.pl -i $genome_file -d $blast_working_dir -p \
+/opt/bio/ncbi-blast+/bin/blastn -db \
+/isilon/biodiversity/reference/ncbi/blastdb/reference/nt/nt -o \
+$genome_file.blast_out -c $num_procs -n $num_splits | grep 'Merge_results' \
+| perl -ne 'if (/Your\sjob\s(\d+)/) { print $1; }'`
+
+parse_jobid=`$qsub_path -N Parse_blast -hold_jid $merge_jobid ./qsubParseBlast.sh $genome_file`
+
+
---
+++ Decontaminate/qsubParseBlast.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#$ -S /bin/bash
+#$ -V
+#$ -M $EMAIL
+#$ -pe orte 1
+#$ -cwd
+
+genome_file=$1
+blast_working_dir=`echo $genome_file | sed 's/\..*/_work/'`
+
+blast_out_file=$genome_file.blast_out.txt
+parsed_blast_file=$blast_out_file.parsed
+sorted_blast_file=$parsed_blast_file.sorted
+
+cat $blast_working_dir/tmp/*-result.txt > $blast_out_file
+cat $genome_file.blast_out.txt | ./parseBlastOutput.pl -s -f > $parsed_blast_file
+cat $parsed_blast_file | cut -f 4,14 | sort -k 2 -t$'\t' -nr > $sorted_blast_file
