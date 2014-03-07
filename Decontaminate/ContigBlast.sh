dfa5e9002101da53c17a416bb7a1638a5e89e631
Jeff Cullis <jeffcullis@gmail.com>
2014-03-07 14:09:56 +0000

    JC - see bug 3097
    git-svn-id: http://svn.biodiversity.agr.gc.ca/repo/source/AssemblyPipeline@4821 8f470391-0214-0410-be57-dbd57b47fce5

--- Decontaminate/ContigBlast.sh
+++ Decontaminate/ContigBlast.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-qsub_path=/opt/gridengine/bin/lx26-amd64/qsub
+qsub_path=/opt/gridengine/bin/linux-x64/qsub
 
 # g is your contigs file
 # p is number of processors per submitted job
 # s is number of files to generate.
 num_procs=8
 num_splits=100
-while getopts g:c:n: flag; do
+blast_bin="/opt/bio/ncbi-blast+/bin/blastn"
+blast_db="/isilon/biodiversity/reference/ncbi/blastdb/reference/nt/nt"
+while getopts g:c:n:p:d: flag; do
     case $flag in
       g)
         genome_file=$OPTARG;
@@ -16,6 +18,12 @@ while getopts g:c:n: flag; do
         ;;
       n)
         num_splits=$OPTARG;
+        ;;
+      p)
+        blast_bin=$OPTARG; # Should be e.g. blastn tblastx,...
+        ;;
+      d)
+        blast_db=$OPTARG; # path to e.g. nr or nt
         ;;
       ?)
         exit;
@@ -28,17 +36,15 @@ mkdir -p $blast_working_dir
 # Setup and run the blast identification, given an input contigs file.
 [ -e qsubParseBlast.sh ] || svn export http://biodiversity/svn/source/AssemblyPipeline/Decontaminate/qsubParseBlast.sh
 [ -e parallel-blast.pl ] || svn export http://biodiversity/svn/source/BlastParallel/parallel-blast.pl
-[ -e parseBlastOutput.pl] || svn export http://biodiversity/svn/source/misc_scripts/parseBlastOutput.pl
+[ -e parseBlastOutput.pl ] || svn export http://biodiversity/svn/source/misc_scripts/parseBlastOutput.pl
 
 chmod +x parallel-blast.pl
 chmod +x parseBlastOutput.pl
 
 # Run parallel-blast.pl using the contigs file as input. 
 merge_jobid=`./parallel-blast.pl -i $genome_file -d $blast_working_dir -p \
-/opt/bio/ncbi-blast+/bin/blastn -db \
-/isilon/biodiversity/reference/ncbi/blastdb/reference/nt/nt -o \
-$genome_file.blast_out -c $num_procs -n $num_splits | grep 'Merge_results' \
-| perl -ne 'if (/Your\sjob\s(\d+)/) { print $1; }'`
+$blast_bin -db $blast_db -o $genome_file.blast_out -c $num_procs \
+-n $num_splits | grep 'Merge_results' | perl -ne 'if (/Your\sjob\s(\d+)/) { print $1; }'`
 
 parse_jobid=`$qsub_path -N Parse_blast -hold_jid $merge_jobid ./qsubParseBlast.sh $genome_file`
 
