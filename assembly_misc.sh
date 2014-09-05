d28b331164500c187ad6fe06260e75dab7bf351f
Jeff Cullis <jeffcullis@gmail.com>
2014-09-05 21:06:45 +0000

    JC: bug 4141
    git-svn-id: http://svn.biodiversity.agr.gc.ca/repo/source/AssemblyPipeline@5482 8f470391-0214-0410-be57-dbd57b47fce5

--- assembly_misc.sh
+++ assembly_misc.sh
@@ -14,9 +14,8 @@ run_spades_PE() {
     submit_host=biocomp-0-9
     [ ! -z $3 ] && prefix=$3
     get_qsub_script
-    # SPAdes uses 16 threads by default.
-    sed -i 's/-pe orte 1/-pe smp 16/' qsub_script.sh 
-    cmd="qsub -N SPAdes_PE -l h=$submit_host -l mem_free=250G qsub_script.sh \"spades.py --careful --pe1-1 $R1 --pe1-2 $R2 -o $prefix\""
+    # SPAdes uses 16 threads and max 250G by default.
+    cmd="qsub -N SPAdes_PE -pe smp 16 -l h=$submit_host -l mem_free=250G qsub_script.sh \"spades.py --careful --pe1-1 $R1 --pe1-2 $R2 -o $prefix\""
     echo $cmd
     eval $cmd
 }
@@ -42,6 +41,43 @@ run_spades_MP() {
     echo $qsub_cmd
     eval $qsub_cmd
 }     
+
+# Run dipSPAdes
+run_dipspades_PE() {
+    R1=$1
+    R2=$2
+    prefix=dipspades_out_PE
+    submit_host=biocomp-0-9
+    [ ! -z $3 ] && prefix=$3
+    get_qsub_script
+    # SPAdes uses 16 threads and max 250G by default.
+    cmd="qsub -N SPAdes_PE -pe smp 16 qsub_script.sh \"dipspades.py --pe1-1 $R1 --pe1-2 $R2 -o $prefix\""
+    echo $cmd
+    eval $cmd
+}
+
+#run_dipspades_PE WG2-Ov-LEV6574-BC6_S1_L001_R1_001.fastq.gz WG2-Ov-LEV6574-BC6_S1_L001_R2_001.fastq.gz Se_LEV6574_spades
+
+# Run with mate-pair data
+# Note that insert sizes are not specified.
+run_dipspades_MP() {
+    PE_R1=$1
+    PE_R2=$2
+    MP3_R1=$3
+    MP3_R2=$4
+    MP8_R1=$5
+    MP8_R2=$6
+    prefix=dipspades_out_P38
+    submit_host=biocomp-0-10
+    [ ! -z $7 ] && prefix=$7
+    get_qsub_script
+    dipspades_cmd="dipspades.py --pe1-1 $PE_R1 --pe1-2 $PE_R2 --mp1-1 $MP3_R1 --mp1-2 $MP3_R2 --mp2-1 $MP8_R1 --mp2-2 $MP8_R2 -o $prefix"
+    # SPAdes uses 16 threads and max 250G by default
+    qsub_cmd="qsub -N SPAdes_MP -pe smp 16 qsub_script.sh \"$dipspades_cmd\""
+    echo $qsub_cmd
+    eval $qsub_cmd
+}     
+
 
 # Reverse-complement a read library
 revcomp() {
@@ -107,6 +143,9 @@ run_bowtie() {
     qsub_sam2bam_out=`eval $qsub_sam2bam_cmd`
     echo $qsub_sam2bam_out
 }
+
+# sort bam
+# index bam
 
 # Run quake
 
