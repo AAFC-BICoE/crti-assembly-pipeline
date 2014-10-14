2be8252f55a69fcadb89d4e3e0abd87c230cdc23
Jacob Jablonski <jjablons@uwaterloo.ca>
2014-10-14 21:16:20 +0000

    Worked on letting velvetg run mutliple kmer jobs
    git-svn-id: http://svn.biodiversity.agr.gc.ca/repo/source/AssemblyPipeline@5525 8f470391-0214-0410-be57-dbd57b47fce5

--- Galaxy/velvet/velvetg_jgi_wrapper.pl
+++ Galaxy/velvet/velvetg_jgi_wrapper.pl
@@ -20,7 +20,6 @@ my $stats_outfile=shift @ARGV or die;
 my $lastgraph_outfile=shift @ARGV or die;
 my $unused_reads_outfile=shift @ARGV or die;
 my $amos_outfile=shift @ARGV or die;
-my $exp_cov_file=shift@ARGV or die;
 
 # setup velvetg folder
 
@@ -41,8 +40,9 @@ for (my $i = 0; $i < $size ; $i ++)
 if ($exp_cov eq "fun_time")
 {
 
+    my $exp_cov_file=shift@ARGV or die;
     open FH, $exp_cov_file;
-    my @array;
+    my @array = ();
     while (<FH>)
     {
 
@@ -51,60 +51,83 @@ if ($exp_cov eq "fun_time")
     }
 
     print @array;
+    print @array[0];
+    my $size_arr = @array;
+    my $r =0;
+    my @new_A = ();
+    for ($r = 0; $r < $size_arr ; $r ++)
+    {
 
+
+
+       @{$array[$r]} = split "\t",  $array[$r];
+    }
+    print "a @array\n";
+    for (my $u = 0; $u < $size_arr ; $u ++)
+    {
+        print $array[$u];
+    }
 
 
     close FH;
 
-}
 
-if(!(-d $velveth_path))
-{
+}else {
 
-	$velveth_path = $velveth_path . "*";
 
-	$velveth_path = ` ls -d $velveth_path | head -n 1`;
 
-}
-chomp $velveth_path;
-print "My velvetg_path is $velvetg_path\n";
-print "My velveth path is $velveth_path\n";
+    if(!(-d $velveth_path))
+    {
 
--d $velvetg_path or mkdir($velvetg_path) or die("Unable to create output folder, $velvetg_path: $!\n");
+       $velveth_path = $velveth_path . "*";
 
-# run command (remaining args, starting with exe path)
-print "The args @ARGV \n";
-print `cp -r $velveth_path/* $velvetg_path`;
+       $velveth_path = ` ls -d $velveth_path | head -n 1`;
 
-open (VELVETG, "@ARGV|") or die("Unable to run velvetg\n");
-open (OUT, ">$velvetg_outfile") or die("Unable to open outfile, $velvetg_outfile: $!\n");
-while (<VELVETG>) {
-    print OUT $_;
-    print if /^Final graph/;
-}
-close VELVETG;
-close OUT;
+    }
+    chomp $velveth_path;
+    #print "My velvetg_path is $velvetg_path\n";
+    #print "My velveth path is $velveth_path\n";
 
-# process output
-unlink($contigs_outfile);
-move("$velvetg_path/contigs.fa", $contigs_outfile);
-unlink($stats_outfile);
-move("$velvetg_path/stats.txt", $stats_outfile);
+    -d $velvetg_path or mkdir($velvetg_path) or die("Unable to create output folder, $velvetg_path: $!\n");
 
-unlink($lastgraph_outfile);
-if ( -f "$velvetg_path/LastGraph") {
-    move("$velvetg_path/LastGraph", $lastgraph_outfile);
-} elsif ( -f "$velvetg_path/Graph2") {
-    move("$velvetg_path/Graph2", $lastgraph_outfile);
-} else {
-   open(OUT, ">$lastgraph_outfile") or die($!);
-   print OUT "ERROR: $velvetg_path/LastGraph not found!\n";
+    # run command (remaining args, starting with exe path)
+    print "The args @ARGV \n";
+    print `cp -r $velveth_path/* $velvetg_path`;
+
+    open (VELVETG, "@ARGV|") or die("Unable to run velvetg\n");
+    open (OUT, ">$velvetg_outfile") or die("Unable to open outfile, $velvetg_outfile: $!\n");
+    while (<VELVETG>) {
+      print OUT $_;
+     print if /^Final graph/;
+    }
+    close VELVETG;
     close OUT;
+
+    # process output
+    unlink($contigs_outfile);
+    move("$velvetg_path/contigs.fa", $contigs_outfile);
+    unlink($stats_outfile);
+    move("$velvetg_path/stats.txt", $stats_outfile);
+
+    unlink($lastgraph_outfile);
+    if ( -f "$velvetg_path/LastGraph") {
+     move("$velvetg_path/LastGraph", $lastgraph_outfile);
+    } elsif ( -f "$velvetg_path/Graph2") {
+     move("$velvetg_path/Graph2", $lastgraph_outfile);
+    } else {
+     open(OUT, ">$lastgraph_outfile") or die($!);
+     print OUT "ERROR: $velvetg_path/LastGraph not found!\n";
+     close OUT;
+   }
+  unlink($unused_reads_outfile);
+  move("$velvetg_path/UnusedReads.fa", $unused_reads_outfile);
+    if ( $amos_outfile ne 'None' ) {
+      unlink($amos_outfile);
+      move("$velvetg_path/velvet_asm.afg", $amos_outfile);
+    }
+  exit;
+
+
+
 }
-unlink($unused_reads_outfile);
-move("$velvetg_path/UnusedReads.fa", $unused_reads_outfile);
-if ( $amos_outfile ne 'None' ) {
-    unlink($amos_outfile);
-    move("$velvetg_path/velvet_asm.afg", $amos_outfile);
-}
-exit;
+
